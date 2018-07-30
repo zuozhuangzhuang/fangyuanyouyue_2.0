8a55c4df1a4e480ef64aae3b5e2eeb18fc031fa4
左壮壮 <zuozhuang_zzz@163.com>
2018-07-30 23:06:30 +0800

    修改了Redis相关代码

--- fangyuanyouyue-microservice/fangyuanyouyue-microservice-goods/src/main/java/com/fangyuanyouyue/goods/controller/AppraisalController.java
+++ fangyuanyouyue-microservice/fangyuanyouyue-microservice-goods/src/main/java/com/fangyuanyouyue/goods/controller/AppraisalController.java
@@ -8,6 +8,7 @@ import com.fangyuanyouyue.base.exception.ServiceException;
 import com.fangyuanyouyue.goods.param.GoodsParam;
 import com.fangyuanyouyue.goods.service.AppraisalService;
 import com.fangyuanyouyue.goods.service.CartService;
+import com.fangyuanyouyue.goods.service.SchedualRedisService;
 import com.fangyuanyouyue.goods.service.SchedualUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -24,7 +25,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(value = "/appraisal")
@@ -40,6 +40,8 @@ public class AppraisalController extends BaseController{
     private CartService cartService;
     @Autowired
     private AppraisalService appraisalService;
+    @Autowired
+    private SchedualRedisService schedualRedisService;
 
     @ApiOperation(value = "申请鉴定", notes = "(void)申请鉴定分为四种情况：1.卖家对自己商品进行鉴定，可显示到商品详情中 " +
             "2.买家对别人的商品进行鉴定，只能自己看到 3.用户上传图片鉴定图片中的物品(这个是全民鉴定还是官方鉴定) " +
@@ -47,9 +49,9 @@ public class AppraisalController extends BaseController{
     @ApiImplicitParams({
             @ApiImplicitParam(name = "token", value = "用户token", required = true, dataType = "String", paramType = "query"),
             @ApiImplicitParam(name = "goodsIds", value = "商品ID数组", allowMultiple = true,dataType = "int", paramType = "query"),
-            @ApiImplicitParam(name = "price", value = "鉴定赏金", dataType = "BigDecimal", paramType = "query"),
             @ApiImplicitParam(name = "description", value = "描述",  dataType = "String", paramType = "query"),
-            @ApiImplicitParam(name = "imgUrl", value = "图片地址",  dataType = "String", paramType = "query")
+            @ApiImplicitParam(name = "imgUrls", value = "图片地址数组",allowMultiple = true, dataType = "String", paramType = "query"),
+            @ApiImplicitParam(name = "videoUrl", value = "视频路径",dataType = "String", paramType = "query")
     })
     @PostMapping(value = "/addAppraisal")
     @ResponseBody
@@ -61,17 +63,18 @@ public class AppraisalController extends BaseController{
             if(StringUtils.isEmpty(param.getToken())){
                 return toError(ReCode.FAILD.getValue(),"用户token不能为空！");
             }
-            Integer userId = (Integer)redisTemplate.opsForValue().get(param.getToken());
+            Integer userId = (Integer)schedualRedisService.get(param.getToken());
             String verifyUser = schedualUserService.verifyUserById(userId);
             JSONObject jsonObject = JSONObject.parseObject(verifyUser);
             if((Integer)jsonObject.get("code") != 0){
                 return toError(jsonObject.getString("report"));
             }
-            redisTemplate.expire(param.getToken(),7, TimeUnit.DAYS);
-            if(StringUtils.isEmpty(param.getTitle())){
-                return toError(ReCode.FAILD.getValue(),"鉴定标题不能为空！");
+            if(param.getGoodsIds() == null){
+                if(param.getImgUrls() == null && param.getVideoUrl() == null){
+                    toError(ReCode.FAILD.getValue(),"至少包含一张图片或一段视频！");
+                }
             }
-            //申请鉴定
+            //TODO 申请鉴定，需要生成订单并返回订单信息
             appraisalService.addAppraisal(userId,param.getGoodsIds(),param.getTitle(), param.getDescription(),param.getPrice(),param.getImgUrl());
             return toSuccess( "申请鉴定成功！");
         } catch (ServiceException e) {
