68119603b6e8fa95d8af5b8c703d1f803775da4f
左壮壮 <zuozhuang_zzz@163.com>
2018-07-30 22:55:31 +0800

    Get改为Post

--- fangyuanyouyue-microservice/fangyuanyouyue-microservice-goods/src/main/java/com/fangyuanyouyue/goods/service/SchedualRedisService.java
+++ fangyuanyouyue-microservice/fangyuanyouyue-microservice-goods/src/main/java/com/fangyuanyouyue/goods/service/SchedualRedisService.java
@@ -1,18 +1,17 @@
-package com.fangyuanyouyue.user.service;
+package com.fangyuanyouyue.goods.service;
 
+import com.fangyuanyouyue.goods.service.impl.SchedualRedisServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.fangyuanyouyue.user.service.impl.SchedualRedisServiceImpl;
-
 @FeignClient(value = "redis-service",fallback = SchedualRedisServiceImpl.class)
 @Component
 public interface SchedualRedisService {
     @RequestMapping(value = "/redis/set",method = RequestMethod.POST)
-    Boolean set(@RequestParam(value = "key") String key,@RequestParam(value = "value") String value,@RequestParam(value = "expire") Long expire);
+    Boolean set(@RequestParam(value = "key") String key, @RequestParam(value = "value") String value, @RequestParam(value = "expire") Long expire);
 
     @RequestMapping(value = "/redis/get",method = RequestMethod.POST)
     Object get(@RequestParam(value = "key") String key);
