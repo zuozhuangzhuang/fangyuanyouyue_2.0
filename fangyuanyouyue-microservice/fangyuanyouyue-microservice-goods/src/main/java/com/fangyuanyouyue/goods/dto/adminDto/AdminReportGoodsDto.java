9280a5e1bbfbd0bf768bb7d13b04584e4f628adc
zuozhuang_zzz@163.com <jueshiwushen>
2018-09-12 22:22:02 +0800

    后台商品举报相关

---
+++ fangyuanyouyue-microservice/fangyuanyouyue-microservice-goods/src/main/java/com/fangyuanyouyue/goods/dto/adminDto/AdminReportGoodsDto.java
@@ -0,0 +1,73 @@
+package com.fangyuanyouyue.goods.dto.adminDto;
+
+import com.fangyuanyouyue.base.util.DateUtil;
+import com.fangyuanyouyue.goods.model.Report;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 商品举报信息Dto
+ */
+@Getter
+@Setter
+@ToString
+public class AdminReportGoodsDto {
+    //举报信息id
+    private Integer id;
+
+    //举报人id
+    private Integer userId;
+
+    //举报人昵称
+    private String nickName;
+
+    //举报人头像
+    private String headImgUrl;
+
+    //被举报商品id
+    private Integer goodsId;
+
+    //被举报商品名
+    private String goodsName;
+
+    //被举报时间
+    private String addTime;
+
+    //处理时间
+    private String updateTime;
+
+    //举报处理状态 1已处理 2未处理
+    private Integer status;
+
+    public AdminReportGoodsDto() {
+    }
+
+    public AdminReportGoodsDto(Report report) {
+        this.id = report.getId();
+        this.userId = report.getUserId();
+        this.nickName = report.getNickName();
+        this.headImgUrl = report.getHeadImgUrl();
+        this.goodsId = report.getBusinessId();
+        this.goodsName = report.getGoodsName();
+        this.addTime = DateUtil.getFormatDate(report.getAddTime(),DateUtil.DATE_FORMT);
+        this.updateTime = DateUtil.getFormatDate(report.getUpdateTime(),DateUtil.DATE_FORMT);
+        this.status = report.getStatus();
+    }
+
+    public static ArrayList<AdminReportGoodsDto> toDtoList(List<Report> list) {
+        if (list == null) {
+            return new ArrayList<>();
+        }
+        ArrayList<AdminReportGoodsDto> dtolist = new ArrayList<>();
+        for (Report model : list) {
+            AdminReportGoodsDto dto = new AdminReportGoodsDto(model);
+            dtolist.add(dto);
+        }
+        return dtolist;
+    }
+
+}
---
+++ fangyuanyouyue-microservice/fangyuanyouyue-microservice-goods/src/test/java/com/fangyuanyouyue/goods/AdminControllerTest.java
@@ -0,0 +1,77 @@
+package com.fangyuanyouyue.goods;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GoodsServiceApplication.class)
+@WebAppConfiguration
+@ContextConfiguration
+@Rollback
+public class AdminControllerTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+    }
+
+
+    /**
+     * 后台处理举报
+     * @throws Exception
+     */
+    @Test
+//    @Transactional
+    public void dealReport() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.put("/adminGoods/dealReport")
+                .param("id","2")
+                .param("content","假货！")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+    /**
+     * 获取举报商品列表
+     * @throws Exception
+     */
+    @Test
+//    @Transactional
+    public void reportList() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/adminGoods/reportList")
+                .param("type","")
+                .param("start","0")
+                .param("limit","10")
+                .param("keyword","")
+                //状态 1已处理 2待处理
+                .param("status","")
+                .param("startDate","2016-01-01 01:01:01")
+                .param("endDate","2018-09-12 16:39:00")
+                .param("orders","add_time")
+                .param("ascType","2")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
+}
