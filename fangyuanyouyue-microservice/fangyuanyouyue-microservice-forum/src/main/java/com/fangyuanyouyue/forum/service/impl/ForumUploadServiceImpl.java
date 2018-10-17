42771fbcdd4e9a89ef5a69ca8f940445a8a186ea
zuozhuang_zzz@163.com <jueshiwushen>
2018-10-17 20:10:24 +0800

    提交测试，正式勿合并

--- fangyuanyouyue-microservice/fangyuanyouyue-microservice-forum/src/main/java/com/fangyuanyouyue/forum/service/impl/ForumUploadServiceImpl.java
+++ fangyuanyouyue-microservice/fangyuanyouyue-microservice-forum/src/main/java/com/fangyuanyouyue/forum/service/impl/ForumUploadServiceImpl.java
@@ -7,7 +7,10 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -47,15 +50,19 @@ public class ForumUploadServiceImpl implements ForumUploadService {
 			//TODO 遍历每个分类
 			for(File element:files) {
 				System.out.println(element.getName());
+				String cloumnType = element.getName().split("-")[0];
+				String cloumnName = element.getName().split("-")[1];
+				System.out.println("专栏分类:【"+cloumnType+"】");
+				System.out.println("专栏名称:【"+cloumnName+"】");
 				if(element.isDirectory()&&!element.getName().startsWith(".")) {
 					//找出分类
 					for(File forum:element.listFiles()) {
 						if(!forum.isDirectory()) {
 							continue;
 						}
-						System.out.println(forum.getName());
 						//解析里面的帖子
 						String title = forum.getName();
+						System.out.println("帖子标题：【"+title+"】");
 						File content = null;
 						List<File> imgs = new ArrayList<File>();
 						for(File details:forum.listFiles()) {
@@ -71,14 +78,14 @@ public class ForumUploadServiceImpl implements ForumUploadService {
 						if(content==null) {
 							System.out.println("内容不存在："+title);
 						}else {
-							if(importForum(title, content, imgs)) {
+							if(importForum(cloumnName,title, content, imgs)) {
 								success ++;
 							}else {
 								error ++;
 							}
 						}
 					}
-					
+
 				}
 			}
 			
@@ -92,88 +99,51 @@ public class ForumUploadServiceImpl implements ForumUploadService {
 	}
 	
 	
-	private boolean importForum(String title,File content,List<File> imgs) {
+	private boolean importForum(String cloumnName,String title,File content,List<File> imgs) {
 		try {
 			
 			System.out.println("导入帖子，标题："+title);
 			
-			String[] titles = title.split("\\-");
+			String[] titles = title.split("-");
 			
-			if(titles.length!=2) {
-				System.err.println("标题不对");
-				return false;
+			if(titles.length==2) {
+				title = titles[1];
 			}
 			
-			ForumColumn column = forumColumnMapper.selectByName(titles[0]);
+			ForumColumn column = forumColumnMapper.selectByName(cloumnName);
 			if(column==null) {
 				System.err.println("找不到专栏");
 				return false;
 			}
 			List<String> urls = new ArrayList<String>();
+			List<ForumDetailDto> details = new ArrayList<ForumDetailDto>();
 			//解析图片
 			for(File img:imgs) {
 				System.out.println("上传图片："+img.getName());
 				String url = fileUploadServiceImpl.uploadPic(img);
 				System.out.println("图片地址："+url);
 				urls.add(url);
+				ForumDetailDto detail = new ForumDetailDto();
+				detail.setType(2);
+				detail.setImgUrl(url);
+				details.add(detail);
 			}
 			
 			BufferedReader buff = new BufferedReader(new InputStreamReader(new FileInputStream(content),codeString(content.getAbsolutePath())));
-			//InputStreamReader is = new InputStreamReader(new FileInputStream(content,"utf-8"));
-	        //BufferedReader br = new BufferedReader(buff);
-			String lineTxt = null; // 读取文件的方法
 			int i = 0;
-			List<ForumDetailDto> details = new ArrayList<ForumDetailDto>();
-			String temp = "";
+			StringBuffer temp = new StringBuffer();
+			String lineTxt = "";
 	        while ((lineTxt = buff.readLine()) != null) {
 	        	System.out.println(lineTxt);
-	        	if(lineTxt.equals("图")) {
-	        		//前面的清除
-	            	ForumDetailDto detail = new ForumDetailDto();
-	        		detail.setType(1);
-	        		detail.setContent(temp);
-	        		details.add(detail);
-	        		temp = "";
-	        		
-	        		//加上图片
-	            	detail = new ForumDetailDto();
-	        		detail.setType(2);
-	        		if(i>urls.size()-1) {
-	    				System.err.println("图片数量有误");
-	        			return false;
-	        		}
-	        		detail.setImgUrl(urls.get(i));
-	        		i ++;
-	            	details.add(detail);
-	            	
-	        	}else {
-	        		temp += lineTxt;
-	        	}
+                temp.append(lineTxt);
+                temp.append("\n");
 	        }
 	        //如果后面还有文字
 	        if(!temp.equals("")) {
 	        	ForumDetailDto detail = new ForumDetailDto();
 	    		detail.setType(1);
-	    		detail.setContent(temp);
+	    		detail.setContent(temp.toString());
 	    		details.add(detail);
-	        }
-	        
-	        while ((lineTxt = buff.readLine()) != null) {
-	        	System.out.println(lineTxt);
-	        	ForumDetailDto detail = new ForumDetailDto();
-	        	if(lineTxt.equals("图")) {
-	        		detail.setType(2);
-	        		if(i>urls.size()-1) {
-	    				System.err.println("图片数量有误");
-	        			return false;
-	        		}
-	        		detail.setImgUrl(urls.get(i));
-	        		i ++;
-	        	}else {
-	        		detail.setType(1);
-	        		detail.setContent(new String(lineTxt));
-	        	}
-	        	details.add(detail);
 	        }
 
 	     // 使用new方法
@@ -184,7 +154,7 @@ public class ForumUploadServiceImpl implements ForumUploadService {
 			
 			ForumInfo info = new ForumInfo();
 			info.setContent(contents);
-			info.setTitle(titles[1]);
+			info.setTitle(title);
 			info.setAddTime(new Date());
 			info.setColumnId(column.getId());
 			info.setIsChosen(2);
@@ -192,7 +162,7 @@ public class ForumUploadServiceImpl implements ForumUploadService {
 			info.setStatus(1);
 			info.setType(1);
 			info.setUserId(column.getUserId());
-			info.setPvCount(0);
+			info.setPvCount((int)(Math.random() * 1000) + 100);
 			
 			System.err.println(info.toString());
 			
